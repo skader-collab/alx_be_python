dbe908070638043967207bc5d707b660ea75ef01
skader-collab <kamarasekous22@gmail.com>
2024-12-12 13:24:14 -0800

    Project python

---
+++ basic_operations.py
@@ -0,0 +1,6 @@
+number1 = 10
+number2 = 5
+ans = number1 + number2
+diff = number1 - number2
+mult = number1 * number2
+print(f'Addition of {number1} and {number2} is {ans} \nSubtraction of {number1} and {number2} is {diff}  \nMultiplication of {number1} and {number2} is {mult}')
