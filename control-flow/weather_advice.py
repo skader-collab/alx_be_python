4a73af06a382daf56508ffbf1eee11b4b6de883b
skader-collab <kamarasekous22@gmail.com>
2024-12-19 15:32:11 -0800

    weather quiz

---
+++ control-flow/weather_advice.py
@@ -0,0 +1,10 @@
+#The weather test
+q1 = str(input('What\'s the weather like today? (sunny/rainy/cold):'))
+if q1 == 'sunny':
+    print('Wear a t-shirt and sunglasses')
+elif q1 == 'rainy':
+    print('Don\'t forget your umbrella and a raincoat')
+elif q1 == 'cold':
+    print('Make sure to wear a warm coat and a scarf.')
+else:
+    print('Sorry, I don\'t have recommendations for this weather.')
